cae0f44cd7a86b4ac3c0bac724475a01256f8dce
add___123 <45610714+itsmeaddof123@users.noreply.github.com>
2026-07-26 00:53:20 -0500

    Add yet another missing semi-colon
    Fix comment formatting in getAuthSettings method.

--- src/main/java/net/earthmc/emcapi/manager/Authorisation.java
+++ src/main/java/net/earthmc/emcapi/manager/Authorisation.java
@@ -31,7 +31,7 @@ public class Authorisation {
     }
 
     public AuthSettings getAuthSettings(UUID owner) {
-        return authMap.get(owner) // This is safe because ConcurrentHashMap will return null if there authMap.containsKey(owner) is false
+        return authMap.get(owner); // This is safe because ConcurrentHashMap will return null if there authMap.containsKey(owner) is false
     }
 
     public void saveAuthSettings(UUID uuid) {
